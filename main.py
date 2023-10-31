f498dcdb3de1dfb57ec79ea8908f33cc87a69367
collinw27 <collin.williams@ufl.edu>
2023-10-31 18:10:38 -0400

    Created decoder function

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# encode() by Spencer Sturman
+# decode() by Collin Williams
+
 def menu():
     print("Menu")
     print("-------------")
@@ -9,22 +12,29 @@ def menu():
 def encode(password):
     new_password = ""
     for digit in password:
-        new_digit = str(int(digit) + 3)
+        new_digit = str((int(digit) + 3) % 10)
         new_password += new_digit
     return new_password
 
+def decode(password):
+    decoded_password = ""
+    for digit in password:
+        decoded_password += str((int(digit) - 3) % 10)
+    return decoded_password
 
 def main():
     choice = 0
+    stored_password = ''
     while choice != 3:
         menu()
         choice = int(input("\nPlease enter an option: "))
         if choice == 1:
             password = input("Please enter your password to encode: ")
             print("Your password has been encoded and stored!\n")
-            encode(password)
+            stored_password = encode(password)
         elif choice == 2:
-            pass
+            original_password = decode(stored_password)
+            print(f"The encoded password is {stored_password}, and the original password is {original_password}.")
         elif choice == 3:
             exit()
 
